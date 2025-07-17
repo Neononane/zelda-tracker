851112261905590c5e1a22ec8e8af80629576d0f
Neononane <ryan@jimmlr.com>
2025-07-17 10:07:22 -0400

    Discord troubleshooting

--- services/discord-control.js
+++ services/discord-control.js
@@ -18,7 +18,7 @@ async function startDiscord() {
     env: {
       ...process.env,
       PULSE_SINK: 'discord_sink',
-      PULSE_SOURCE: 'discord_mic'
+      PULSE_SOURCE: 'discord_sink.monitor'
     },
     args: [
       '--no-sandbox',
